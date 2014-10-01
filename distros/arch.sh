f1714367051ebc518ba0df0cca59d1c184e34dd4
testi torax <torax@testi.com>
2014-10-02 00:07:17 +0300

    WIP: add iso download and md5sum match to arch.sh

--- distros/arch.sh
+++ distros/arch.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
 #description: arch linux
-echo "asdgfg"
+
+#TMPDIR=$(mktemp -d)
+TMPDIR=/tmp/archlinux
+mkdir $TMPDIR
+cd $TMPDIR
+wget -c http://ftp.eenet.ee/pub/archlinux/iso/2014.10.01/archlinux-2014.10.01-dual.iso
+wget -c http://ftp.eenet.ee/pub/archlinux/iso/2014.10.01/md5sums.txt
+
+grep archlinux-2014.10.01-dual.iso md5sums.txt > md5sum.txt
+md5sum -c md5sum.txt
+
+mkdir -p $TMPDIR/mount 
+
+sudo mount -o loop,ro archlinux-2013.11.01-dual.iso /$TMPDIR/mount
+
+
+
